98b4104cbd981f03c2ac172303e4c1249e5d3f1f
Igor Zelmanovich <igor.israel@gmail.com>
2012-05-22 13:54:31 +0300

    add missing file

---
+++ ReleaseIt.Web/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReleaseIt.Web
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<SelectListItem> GetSelectListItem<T>() 
+        {
+            return System.Enum.GetValues(typeof(T)).Cast<T>().Select(x =>
+                new SelectListItem
+                {
+                    Text = x.ToString(),
+                    Value = Convert.ToInt32(x).ToString(),
+                }
+
+            );
+
+
+        }
+    }
+}
